2136810c2ec3082ea06e4efe89fa4815a5364fd7
ReubenShilling <chiefthunderfoot@gmail.com>
2021-03-30 11:47:03 -0500

    Add chroma.js
    Replaced custom code to calculate color differences with chroma.js
    library functions.

--- js/index.js
+++ js/index.js
@@ -9,42 +9,34 @@ $(function () {
         if (event.type === "keypress" && event.key === "Enter")
             event.preventDefault();
         const numGroup = parseInt($(this).val());
-        const minInput = parseInt($(this).attr("min"));
-        const maxInput = parseInt($(this).attr("max"));
-        if (numGroup >= minInput && numGroup <= maxInput) {
-            const colorGroup = Math.round(numGroup / 3);
-            const interval = Math.round(255 / colorGroup);
-            const $tbody = $("#colorGradientTable tbody");
-            $tbody.find("tr").remove();
+        const c1 = chroma('cyan');
+        const c2 = chroma('lime');
+        const c3 = chroma('yellow');
+        const c4 = chroma('red');
+        const $tbody = $("#colorGradientTable tbody");
 
-            // loop for iterating over range to create gradients
-            for (let i = 0; i < numGroup; i++) {
-                const $curRow = $tbody.append(
-                    `<tr>
+        // clear the table
+        $tbody.find("tr").remove();
+
+        // loop for iterating over range to create gradients
+        for (let i = 0; i < numGroup; i++) {
+            const $curRow = $tbody.append(
+                `<tr>
                     <td class="border border-dark"></td>
                     <td class="border border-dark"></td>
                     <td></td>
                 </tr>`)
-                    .find('tr:last');
-                const $indexCell = $curRow.find('td:eq(0)');
-                const $valCell = $curRow.find('td:eq(1)');
-                const $colorCell = $curRow.find('td:eq(2)');
-                let rgbString = '';
-                switch (true) {
-                    case (i < colorGroup):
-                        rgbString = `0, 255, ${Math.max((255 - interval) - (i * interval), 0)}`;
-                        break;
-                    case (i < colorGroup * 2):
-                        rgbString = `${Math.max(((i - colorGroup) * interval) + interval, 0)}, 255, 0`;
-                        break;
-                    default:
-                        rgbString = `255, ${Math.max((255 - interval) - ((i - (colorGroup * 2)) * interval), 0)}, 0`;
-                        break;
-                }
-                $indexCell.html(i + 1);
-                $valCell.html(rgbString);
-                $colorCell.css("background-color", `rgb(${rgbString})`);
-            }
+                .find('tr:last');
+            const $indexCell = $curRow.find('td:eq(0)');
+            const $valCell = $curRow.find('td:eq(1)');
+            const $colorCell = $curRow.find('td:eq(2)');
+            const factor = (i + 1) / numGroup;
+
+            // let chroma do the magic...
+            const rgbString = chroma.scale([c1, c2, c3, c4])(factor).rgb().toString();
+            $indexCell.html(i + 1);
+            $valCell.html(rgbString);
+            $colorCell.css("background-color", `rgb(${rgbString})`);
         }
     });
 });
